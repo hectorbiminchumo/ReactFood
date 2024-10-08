dbdfe4210e6c18b82a2485023613e156240750f2
Hector <hectorbiminchumo@gmail.com>
2024-10-08 12:13:25 -0500

    make an object from input entries

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -16,9 +16,18 @@ function Checkout() {
       userProgressCtx.hideCheckout();
     }
 
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const fd = new FormData(event.target);
+      
+      const customerData = Object.fromEntries(fd.entries()); // { enamil: test@example.com }
+      console.log(customerData);
+    }
+
   return (
     <Modal open={userProgressCtx.progress ==='checkout'} onClose={handleClose}>
-    <form>
+    <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
 
