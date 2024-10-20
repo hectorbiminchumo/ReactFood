4fb203e9b60743e9823d55b74f373c67d3eb69d7
Hector <hectorbiminchumo@gmail.com>
2024-10-19 23:10:20 -0500

    handling error when fetching data fail

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,33 @@
-import React, { useEffect, useState } from 'react'
+
 import MealItem from './MealItem';
+import useHttp from './hooks/useHttp';
+import Error from './Error';
 
-
+const requestConfig = {};
 function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    const { 
+        data: loadedMeals,
+        isLoading, 
+        error 
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
     
-    useEffect(() => {
-    async function fetchMeals() {
-        
-        const response = await fetch('http://localhost:3000/meals')
-        
-        if(!response.ok) {
-            //..
-        }
-        const meals  = await response.json()
-        setLoadedMeals(meals);
+    
+
+    if (isLoading) {
+        return(
+            <p className="center">Fetching meals...</p>
+        )
     }
-        fetchMeals()
-    },[]);
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />
+    }
+
+    // if (!data) {
+    //     return <p>No meals found.</p>
+    // }
+    
 
     return (
         <ul id="meals">
